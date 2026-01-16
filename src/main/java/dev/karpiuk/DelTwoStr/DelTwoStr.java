760b5c429b7f7bf6d05cc3f07ca37bec2e0b00b3
olek2303 <olek2303@gmail.com>
2026-01-16 19:50:23 +0100

    add new solution

---
+++ src/main/java/dev/karpiuk/DelTwoStr/DelTwoStr.java
@@ -0,0 +1,37 @@
+package dev.karpiuk.DelTwoStr;
+
+public class DelTwoStr {
+
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            int prev = 0;
+            for (int j = 1; j <= n; j++) {
+                int temp = dp[j];
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[j] = prev + 1;
+                } else {
+                    dp[j] = Math.max(dp[j], dp[j - 1]);
+                }
+                prev = temp;
+            }
+        }
+
+        int lcs = dp[n];
+        return (m - lcs) + (n - lcs);
+    }
+
+    public static void main(String[] args) {
+        String s1 = "sea";
+        String s2 = "eat";
+
+        DelTwoStr dts = new DelTwoStr();
+        int num = dts.minDistance(s1, s2);
+        System.out.println(num);
+    }
+
+}
